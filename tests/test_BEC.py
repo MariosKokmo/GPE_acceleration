607daf17ce12b8b160c893697ef68905b11bf8e2
MariosKokmo <pjpastakijuniormarios@yahoo.gr>
2025-03-19 18:41:57 +0000

    Updates

--- tests/test_BEC.py
+++ tests/test_BEC.py
@@ -2,29 +2,49 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+import unittest
 sys.path.append(".")
 from src.models import BEC as BEC
+import src.library.gpe_library as gpe
+import src.library.ground_state as gs
+import src.utils.read_write_utils as rw
+from src.utils import video_creation
+import numpy as np
+import os
+import torch
+from pathlib import Path
+from sys import platform
 
-bec = BEC.BEC()
 
-imprint_position_x = np.array([[[0],[0],[0]]])
-imprint_position_y = np.array([[[0],[0],[0]]])
-imprinting_charge = np.array([[[1],[1],[1]]])
-imprint_times = np.array([[7,10,15]])
+class TestBEC(unittest.TestCase):
+    def test_create_vortex_list_single(self):
+        bec = BEC.BEC()
 
-vortex_array = bec._create_vortex_list(imprint_position_x, imprint_position_y, imprinting_charge, imprint_times)
-print(vortex_array)
+        imprint_position_x = np.array([[[0], [0], [0]]])
+        imprint_position_y = np.array([[[0], [0], [0]]])
+        imprinting_charge = np.array([[[1], [1], [1]]])
+        imprint_times = np.array([[7, 10, 15]])
 
-bec._calculate_all_phases(vortex_array)
+        vortex_array = bec._create_vortex_list(imprint_position_x, imprint_position_y, imprinting_charge, imprint_times)
+        self.assertIsNotNone(vortex_array)  # Example assertion
+        print(vortex_array)
+        bec._calculate_all_phases(vortex_array)
+        print(bec.all_phases)
 
-imprint_position_x = np.array([[[0,0],[0]]])
-imprint_position_y = np.array([[[0,0],[0]]])
-imprinting_charge = np.array([[[1,2],[1]]])
-imprint_times = np.array([[7,9]])
+    def test_create_vortex_list_multiple(self):
+        bec = BEC.BEC()
 
-vortex_array = bec._create_vortex_list(imprint_position_x, imprint_position_y, imprinting_charge, imprint_times)
-print(vortex_array)
+        imprint_position_x = np.array([[[0, 0], [0]]])
+        imprint_position_y = np.array([[[0, 0], [0]]])
+        imprinting_charge = np.array([[[1, 2], [1]]])
+        imprint_times = np.array([[7, 9]])
 
-bec._calculate_all_phases(vortex_array)
+        vortex_array = bec._create_vortex_list(imprint_position_x, imprint_position_y, imprinting_charge, imprint_times)
+        self.assertIsNotNone(vortex_array)  # Example assertion
+        print(vortex_array)
 
-print(f"{imprint_times}")
+        bec._calculate_all_phases(vortex_array)
+        print(bec.all_phases)
+
+if __name__ == "__main__":
+    unittest.main()
